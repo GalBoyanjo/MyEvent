738e3e2749e04f6ad91f3f4fcb5cd07c2eb0e302
gal <galboyanjo@gmail.com>
2017-05-21 15:16:42 +0300

    create-invitation activity

--- app/src/main/java/com/gal/invitation/Screens/CreateInvitation.java
+++ app/src/main/java/com/gal/invitation/Screens/CreateInvitation.java
@@ -1,63 +1,152 @@
 package com.gal.invitation.Screens;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.gal.invitation.R;
 
 public class CreateInvitation extends AppCompatActivity {
+    EditText date;
+    EditText time;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_invitation);
+
+
+        final Spinner eventType = (Spinner) findViewById(R.id.eventType);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(CreateInvitation.this,
+                R.array.type, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        eventType.setAdapter(adapter);
+
+
+        eventType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                eventSelected(eventType);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+
+            }
+        });
+
+
+        date = (EditText) findViewById(R.id.eventDate);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int eventYear = calendar.get(Calendar.YEAR);
+                int eventMonth = calendar.get(Calendar.MONTH);
+                int eventDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+                datePickerDialog = new DatePickerDialog(CreateInvitation.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int day) {
+                                date.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, eventYear, eventMonth, eventDay);
+                datePickerDialog.show();
+            }
+        });
+
+        time = (EditText) findViewById(R.id.eventTime);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar eventTime = Calendar.getInstance();
+                int hour = eventTime.get(Calendar.HOUR_OF_DAY);
+                int minute = eventTime.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(CreateInvitation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        if(selectedMinute<10)
+                            time.setText((selectedHour + ":" + "0" + selectedMinute));
+                        else
+                            time.setText((selectedHour + ":" + selectedMinute));
+                    }
+                }, hour, minute, true);
+                timePickerDialog.setTitle("Select Time");
+                timePickerDialog.show();
+            }
+        });
+
+        final Spinner eventPlaceType = (Spinner) findViewById(R.id.eventPlaceType);
+
+        ArrayAdapter<CharSequence> placeTypeAdapter = ArrayAdapter.createFromResource(CreateInvitation.this,
+                R.array.placeType, android.R.layout.simple_spinner_item);
+
+        placeTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        eventPlaceType.setAdapter(placeTypeAdapter);
+
+
+        eventPlaceType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                eventPlaceTypeSelected(eventPlaceType);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+
+            }
+        });
+
+
+
     }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstannceState){
-        AlertDialog.Builder builder = new AlertDialog.Builder(CreateInvitation.this);
-        builder.setView(contactEditView);
+    public void eventSelected(Spinner eventType){
+        String type = eventType.getSelectedItem().toString();
+        EditText weddingGroom = (EditText) findViewById(R.id.eventGroom);
+        EditText weddingBride = (EditText) findViewById(R.id.eventBride);
+        EditText weddingParents = (EditText) findViewById(R.id.eventParents);
 
-        final EditText contactName = (EditText) contactEditView.findViewById(R.id.contact_name);
-        final EditText contactPhone = (EditText) contactEditView.findViewById(R.id.contact_phone);
+        Toast.makeText(CreateInvitation.this,
+                type,
+                Toast.LENGTH_LONG).show();
 
-        contactName.setHint(String.valueOf(contact.getName()));
-        contactPhone.setHint(String.valueOf(contact.getPhone()));
+        if (type.contentEquals("Wedding")){
+            weddingGroom.setVisibility(View.VISIBLE);
+            weddingBride.setVisibility(View.VISIBLE);
+            weddingParents.setVisibility(View.GONE);
+        }
+        if (type.contentEquals("Other")) {
+            weddingGroom.setVisibility(View.GONE);
+            weddingBride.setVisibility(View.GONE);
+            weddingParents.setVisibility(View.VISIBLE);
+        }
+    }
 
-        builder
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String setContactName = contactName.getText().toString();
-                        if (TextUtils.isEmpty(setContactName))
-                            setContactName = (String.valueOf(contact.getName()));
-                        String setContactPhone = contactPhone.getText().toString();
-                        if (TextUtils.isEmpty(setContactPhone))
-                            setContactPhone = (String.valueOf(contact.getPhone()));
-                        editContact(contact, setContactName, setContactPhone);
-                        adapter.remove(contact);
-                        contact.setName(setContactName);
-                        contact.setPhone(setContactPhone);
-                        adapter.add(contact);
-                        adapter.notifyDataSetChanged();
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-
-                    }
-                });
-        builder.create();
-
-        builder.show();
-
+    public void eventPlaceTypeSelected(Spinner eventPlaceType){
 
     }
 }
